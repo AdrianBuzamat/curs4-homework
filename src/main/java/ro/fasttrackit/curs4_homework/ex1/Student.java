d5dbd8adaa4cdfa6c481209afced1669954c6e7b
Adrian Buzamat <adrian.buzamat@yahoo.com>
2022-06-01 19:50:50 +0300

    Initial commit

---
+++ src/main/java/ro/fasttrackit/curs4_homework/ex1/Student.java
@@ -0,0 +1,10 @@
+package ro.fasttrackit.curs4_homework.ex1;
+
+import java.time.LocalDate;
+import static java.util.random.RandomGenerator.*;
+
+public record Student(String name, LocalDate birthday, Double grade) {
+    public Student(String name, int age, double grade){
+        this(name, LocalDate.of(age, getDefault().nextInt(1, 12),getDefault().nextInt(1, 31)), grade);
+    }
+}
